0815a9d489869a7a69ad579c763d74d4dcbfa33a
SammyKenzi <108962885+SammyKenzi@users.noreply.github.com>
2024-01-24 15:01:50 +0900

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for y in range(len(self.board)):
-                for x in range(len(self.board[y])):
-                    if self.board[y][x] != 0:
-                        pygame.draw.rect(self.screen, (255, 255, 255), (x * self.block_size, y * self.block_size, self.block_size, self.block_size))
